ac4d0933c0e940af6d68c94f854f91f81931a83d
Leon <Leonhuber2010@gmail.com>
2026-08-03 01:06:03 +0200

    sign out button

--- src/screens/tabs/profile.tsx
+++ src/screens/tabs/profile.tsx
@@ -1,16 +1,45 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import auth from '@react-native-firebase/auth';
 
 
 
-export default function Profile() {
 
+export default function Profile({navigation}: any) {
+    const insets = useSafeAreaInsets()
+
+    const logOut = () => {
+        auth().signOut()
+        navigation.navigate("Login")
+    }
 
     return (
-        <View>
+        <View style={[styles.container, {paddingTop: insets.top + 16}]}>
             <Text>Profile Screen</Text>
-            <TouchableOpacity>
-                <Text>Add</Text>
+            <TouchableOpacity style={styles.logOutButton} onPress={() => logOut()}>
+                <Text style={styles.buttonText}>Log out</Text>
             </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        gap: 20,
+        padding: 10,
+    },
+
+    logOutButton: {
+        backgroundColor: '#3B82F6',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        color: '#fff',
+    },
+})
